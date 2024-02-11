01509c808f8648e47c34a9fbc91ac8ddc116e9fe
Burnside21140 <21140@burnside.school.nz>
2024-02-12 11:10:59 +1300

    Cannon firing Function works for all ships

--- Pirasea/Scripts/game.gd
+++ Pirasea/Scripts/game.gd
@@ -6,23 +6,21 @@ var cannonballxv = 0
 var cannonballyv = 0
 
 func _on_cannonball_timeout():
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn1.global_position, "left")
-	print($Player/SpawnHandler/Spawn1.position)
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn2.global_position, "left")
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn3.global_position, "right")
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn4.global_position, "right")
+	_spawn_cannon_ball($Player/SpawnHandler/Spawn1.global_position, $Player/SpawnHandler/Spawn1.position)
+	_spawn_cannon_ball($Player/SpawnHandler/Spawn2.global_position, $Player/SpawnHandler/Spawn2.position)
+	_spawn_cannon_ball($Player/SpawnHandler/Spawn3.global_position, $Player/SpawnHandler/Spawn3.position)
+	_spawn_cannon_ball($Player/SpawnHandler/Spawn4.global_position, $Player/SpawnHandler/Spawn4.position)
 
 
 func _spawn_cannon_ball(spawn_location, side):
+	print(side)
 	var cannonballinstance = cannonball.instantiate()
 	add_child(cannonballinstance)
 	cannonballinstance.position = spawn_location
-	if side == "left":
+	if side[1] < 0:
 		cannonballxv = cos($Player.rotation + 4.712385)*200
 		cannonballyv = sin($Player.rotation + 4.712385)*200
-	if side == "right":
+	if side[1] > 0:
 		cannonballxv = cos($Player.rotation + 1.570795)*200
 		cannonballyv = sin($Player.rotation + 1.570795)*200
 	cannonballinstance.linear_velocity = Vector2(cannonballxv,cannonballyv)
-
-#instead of using left and right, read the y value, if y is negative, do left spawning.
