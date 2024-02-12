6ec931a7b41a29f23bcb71347a0f651b2a7e2aea
21140Burnside <21140@burnside.school.nz>
2024-02-12 19:35:52 +1300

    Cannon functions work for ALL player ships

--- Pirasea/Scripts/game.gd
+++ Pirasea/Scripts/game.gd
@@ -6,14 +6,10 @@ var cannonballxv = 0
 var cannonballyv = 0
 
 func _on_cannonball_timeout():
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn1.global_position, $Player/SpawnHandler/Spawn1.position)
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn2.global_position, $Player/SpawnHandler/Spawn2.position)
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn3.global_position, $Player/SpawnHandler/Spawn3.position)
-	_spawn_cannon_ball($Player/SpawnHandler/Spawn4.global_position, $Player/SpawnHandler/Spawn4.position)
-
+	for i in get_node("Player/SpawnHandler").get_children():
+		_spawn_cannon_ball(i.global_position, i.position)
 
 func _spawn_cannon_ball(spawn_location, side):
-	print(side)
 	var cannonballinstance = cannonball.instantiate()
 	add_child(cannonballinstance)
 	cannonballinstance.position = spawn_location
